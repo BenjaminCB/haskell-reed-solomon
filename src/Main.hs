32fc52a45b79bd42805124804ce6885bb3c83eb5
Ace_BCB <benjamincb@hotmail.dk>
2021-06-29 12:46:00 +0200

    made some small changes to the execcuteable file

--- src/Main.hs
+++ src/Main.hs
@@ -1,10 +1,17 @@
 module Main where
 
-import ReedSolomon
+import qualified ReedSolomon as RS
+import qualified ErrorInjection as EI
+import qualified Config as C
 
 import System.IO
 
 main :: IO ()
 main = do
-    x <- getLine
-    print $ encode x
+    line <- getLine
+    let encoded = RS.encode line
+    print encoded
+    let err = EI.polyBurstErrorInjection encoded C.t
+    print err
+    let decoded = RS.decode err
+    print decoded
